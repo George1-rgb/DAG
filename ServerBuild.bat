feada836a1d051ae6015590908f442351672f8cb
Гоцеридзе Георгий <gotseridze.george@csoft.ru>
2025-07-14 10:55:38 +0300

    fixserv

--- ServerBuild.bat
+++ ServerBuild.bat
@@ -1,7 +1,7 @@
 C:\UE\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
- -project="H:/PROJECTS/DAG/DAG/DAG.uproject" ^
+ -project="H:/PROJECTS/DAG_SERVER/DAG/master/DAG.uproject" ^
  -server ^
  -serverconfig=Development ^
  -cook -allmaps -build -stage -pak -archive ^
- -archivedirectory="H:/PROJECTS/DAG/Build" ^
+ -archivedirectory="H:/PROJECTS/DAG_SERVER/DAG/Build" ^
  -platform=Win64
