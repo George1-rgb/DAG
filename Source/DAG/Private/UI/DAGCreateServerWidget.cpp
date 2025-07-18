d3a44afc32480647928d92080187a4f93242bc1d
George <ggoceridze@bk.ru>
2025-07-18 15:34:23 +0300

    load

---
+++ Source/DAG/Private/UI/DAGCreateServerWidget.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/DAGCreateServerWidget.h"
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/CheckBox.h"
---
+++ Source/DAG/Public/UI/DAGCreateServerWidget.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/DAGBaseWidget.h"
+#include "DAGCreateServerWidget.generated.h"
+
+/**
+ * 
+ */
+class UButton;
+class UEditableTextBox;
+class UCheckBox;
+UCLASS()
+class DAG_API UDAGCreateServerWidget : public UDAGBaseWidget
+{
+	GENERATED_BODY()
+//	UDAGCreateServerWidget() {}
+//protected:
+//	virtual void NativeConstruct() override;
+//protected:
+//
+//	UPROPERTY(meta = (BindWidget))
+//	UButton* Btn_CreateRoom;
+//
+//	UPROPERTY(meta = (BindWidget))
+//	UEditableTextBox* TB_RoomName;
+//
+//	UPROPERTY(meta = (BindWidget))
+//	UEditableTextBox* TB_NumPlayers;
+//
+//	UPROPERTY(meta = (BindWidget))
+//	UCheckBox* TB_NumPlayers;
+//
+//private:
+//	UFUNCTION()
+//	void On_CreateRoom();
+};
